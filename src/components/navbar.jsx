c59643c0886dcae4701d6db691fd6936e5b8cdbb
shoxaka <shoxaka@gmail.com>
2024-10-04 10:48:19 +0500

    error

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -40,21 +40,30 @@ export default function Example() {
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
             <div className="hidden sm:ml-6 sm:block">
               <div className="flex space-x-4">
-                {navigation.map((item) => (
-                  <Link
-                    key={item.name}
-                    to={item.href}
-                    aria-current={item.current ? "page" : undefined}
-                    className={classNames(
-                      item.current
-                        ? "bg-gray-900 text-white"
-                        : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                      "rounded-md px-3 py-2 text-sm font-medium"
-                    )}
-                  >
-                    {item.name}
-                  </Link>
-                ))}
+                <Link
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+                  to={`/`}
+                >
+                  Home
+                </Link>
+                <Link
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+                  to={`/about`}
+                >
+                  About
+                </Link>
+                <Link
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+                  to={`/skills`}
+                >
+                  Skills
+                </Link>
+                <Link
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+                  to={`/portfolio`}
+                >
+                  Portfolio
+                </Link>
               </div>
             </div>
           </div>
